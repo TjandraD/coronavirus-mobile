e78fb65b1958e49544f2dc144f13f99fe22cb144
TjandraD <t.darmo.t@gmail.com>
2021-01-15 09:10:22 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:covid19_stats/stats_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'COVID-19 Stats',
+      home: StatsPage(),
+    );
+  }
+}
